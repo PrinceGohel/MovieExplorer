5e50c40f9be4eeed57fa300ea6965e61c5df1797
PrinceGohel <princegohel.dev@gmail.com>
2026-08-21 17:47:05 +0530

    deleted old version of website

--- main.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.getElementById("ulist");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
